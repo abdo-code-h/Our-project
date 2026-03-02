9c5af77995050d2d4d4b1edda3222ef16fc689a8
abdo-code-h <hammiabderrahim7@gmail.com>
2026-03-02 14:28:24 +0100

    Revert "Merge the add feature branch"
    This reverts commit c755fc6b1ce1f47dee730b37dd59b36a5cbb74dd.

--- testing.py
+++ testing.py
@@ -1,8 +1,5 @@
 name = input("Enter your name: ")
-age = int(input("What is the year that you was born: "))
 if name:
     print(f"Hi {name} how are you!")
-    if age:
-        print(f"You have {- age + 2026} years old")
 else:
     print("Please enter your name")
