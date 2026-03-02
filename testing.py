c755fc6b1ce1f47dee730b37dd59b36a5cbb74dd
abdo-code-h <hammiabderrahim7@gmail.com>
2026-03-02 14:16:35 +0100

    Merge the add feature branch

--- testing.py
+++ testing.py
@@ -1,5 +1,8 @@
 name = input("Enter your name: ")
+age = int(input("What is the year that you was born: "))
 if name:
     print(f"Hi {name} how are you!")
+    if age:
+        print(f"You have {- age + 2026} years old")
 else:
     print("Please enter your name")
